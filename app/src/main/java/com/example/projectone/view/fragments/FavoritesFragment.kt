ec0a4ea53e99bc20076f730791fa54fd6eca571d
MarcosAndre28 <marcosandre1728@gmail.com>
2023-01-08 18:50:55 -0300

    Aba de favoritos

---
+++ app/src/main/java/com/example/projectone/view/fragments/FavoritesFragment.kt
@@ -0,0 +1,20 @@
+package com.example.projectone.view.fragments
+
+import android.os.Bundle
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+import com.example.kotlindls.databinding.FavoritesFragmentBinding
+import com.example.projectone.base.BaseFragment
+
+class FavoritesFragment : BaseFragment<FavoritesFragmentBinding>() {
+
+    override fun createBinding(inflater: LayoutInflater, container: ViewGroup?): FavoritesFragmentBinding {
+        return FavoritesFragmentBinding.inflate(inflater,container,false)
+    }
+
+    override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
+        super.onViewCreated(view, savedInstanceState)
+
+    }
+}
